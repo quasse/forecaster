c932085e8e2e8f414fcb781859d541fdd665fbf7
Sam Morgen <samorgen14@gmail.com>
2021-06-05 13:21:12 -0700

    Added city header and icon on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
+var currentWeatherEl = document.querySelector("#city-condition");
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
@@ -31,6 +32,23 @@ var getForecast = function (cityInput) {
 
 var displayWeather = function (data) {
   console.log(data);
+  //Clear HTML
+  currentWeatherEl.innerHTML = "";
+
+  //Add Header
+  var cityHeaderEl = document.createElement("h3");
+  var cityTitle = data.name + " DATE ";
+
+  cityHeaderEl.textContent = cityTitle;
+
+  currentWeatherEl.append(cityHeaderEl);
+
+  //Add icon
+  var weatherIconEl = document.createElement("img");
+  var iconCode = data.weather[0].icon;
+  var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
+  weatherIconEl.setAttribute("src", iconUrl);
+  currentWeatherEl.append(weatherIconEl);
 };
 
 var displayForecast = function (data) {};
