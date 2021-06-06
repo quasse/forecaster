9c5c6a9806bb77dd97a9603246e38f003a3ab4eb
Sam Morgen <samorgen14@gmail.com>
2021-06-05 17:10:19 -0700

    Added weather elements to weather box

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
 var userFormEl = document.querySelector("#user-form");
 var cityInputEl = document.querySelector("#city");
 var currentWeatherEl = document.querySelector("#city-condition");
+var forecastEl = document.querySelector("#city-forecast");
 
+//Handles submission for button
 var formSubmitHandler = function (event) {
   event.preventDefault();
   var userInput = cityInputEl.value.trim();
@@ -14,6 +16,7 @@ var formSubmitHandler = function (event) {
   }
 };
 
+//API calls to OpenWeatherMap
 var getForecast = function (cityInput) {
   var apiKey = "0f6469e694030cd77754702ac512abfd";
   var apiUrl =
@@ -21,36 +24,135 @@ var getForecast = function (cityInput) {
     cityInput +
     "&units=imperial&appid=" +
     apiKey;
+
+  //API call to get initial weather data
   fetch(apiUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      displayWeather(data);
+      var apiUrl2 =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        data.coord.lat +
+        "&lon=" +
+        data.coord.lon +
+        "&appid=" +
+        apiKey;
+      //second API call to get UVI index
+      fetch(apiUrl2)
+        .then(function (response2) {
+          return response2.json();
+        })
+        .then(function (data2) {
+          displayWeather(data, data2);
+        });
     });
 };
 
-var displayWeather = function (data) {
-  console.log(data);
+//Function to display the weather on right side of the screen
+var displayWeather = function (data, data2) {
   //Clear HTML
   currentWeatherEl.innerHTML = "";
 
+  var headerRow = document.createElement("div");
+  headerRow.classList = "row";
+
+  //get date
+  var date = formatDate(data.dt);
+
   //Add Header
   var cityHeaderEl = document.createElement("h3");
-  var cityTitle = data.name + " DATE ";
-
+  cityHeaderEl.classList = "col-12 pt-2";
+  var cityTitle = data.name + ", " + date;
   cityHeaderEl.textContent = cityTitle;
-
-  currentWeatherEl.append(cityHeaderEl);
 
   //Add icon
   var weatherIconEl = document.createElement("img");
   var iconCode = data.weather[0].icon;
   var iconUrl = "https://openweathermap.org/img/w/" + iconCode + ".png";
   weatherIconEl.setAttribute("src", iconUrl);
-  currentWeatherEl.append(weatherIconEl);
+
+  //Put icon in header
+  cityHeaderEl.append(weatherIconEl);
+
+  //Put header in row
+  headerRow.append(cityHeaderEl);
+
+  //put row in weather container
+  currentWeatherEl.append(headerRow);
+
+  //Create row for details
+  var infoRow = document.createElement("div");
+  infoRow.classList = "row";
+
+  //get temperature
+  var temp = data.main.temp;
+
+  //Add temperature to the infobox
+  var tempEl = document.createElement("div");
+  tempEl.classList = "col-12";
+  tempEl.textContent = "Temp: " + data.main.temp + "°F";
+  infoRow.append(tempEl);
+
+  //get wind
+  var wind = data.wind.speed;
+
+  //Add wind to the infobox
+  var windEl = document.createElement("div");
+  windEl.classList = "col-12";
+  windEl.textContent = "Wind: " + data.wind.speed + "MPH";
+  infoRow.append(windEl);
+
+  //get humidity
+  var humidity = data.main.humidity;
+
+  //add humidity to the infobox
+  var humidityEl = document.createElement("div");
+  humidityEl.classList = "col-12";
+  humidityEl.textContent = "Humidity: " + data.main.humidity + "%";
+  infoRow.append(humidityEl);
+
+  //TODO get UV index
+  var uvEl = document.createElement("div");
+  uvEl.classList = "col-12";
+  uvEl.textContent = "UV Index: " + data2.current.uvi;
+  infoRow.append(uvEl);
+
+  //Add all rows to infobox
+  currentWeatherEl.append(infoRow);
+
+  displayForecast(data2);
 };
 
-var displayForecast = function (data) {};
+// Method to formate date
+var formatDate = function (timeStamp) {
+  var date = new Date(timeStamp * 1000);
+  var dateString = date.toDateString();
+  return dateString;
+};
+
+var displayForecast = function (data) {
+  console.log(data);
+
+  for (var i = 0; i < data.daily.length; i++) {
+    var date = formatDate(data.daily[i].dt);
+    console.log(date);
+  }
+
+  //Clear HTML in case search has been done before
+  forecastEl.innerHTML = "";
+
+  //Create row for content
+  var forecastRow = document.createElement("div");
+  forecastRow.classList = "row";
+
+  //create col for header
+  var headerEl = document.createElement("h3");
+  headerEl.classList = "col-12 mt-2";
+  headerEl.textContent = "5-Day Forecast:";
+  forecastRow.append(headerEl);
+
+  forecastEl.append(forecastRow);
+};
 
 userFormEl.addEventListener("submit", formSubmitHandler);
